58cd62b6f0b41dd28354ff469bec6f427f3d311e
bereket-kume <bereketkume@gmail.com>
2024-02-27 13:52:44 -0800

    done

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# A regular expression that is matches a given pattern
+pattern= /\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/
+
+input = ARGV[0]
+
+matches = input.scan(pattern)
+
+puts matches.join.(',')

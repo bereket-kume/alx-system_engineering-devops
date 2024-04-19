7ba7ef53c2ceccea34a6bf4568c6e8758d0ee2c8
bereket-kume <bereketkume@gmail.com>
2024-04-19 14:03:10 -0700

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
+# create a file in /tmp
 file { '/tmp/school':
-	ensure => file,
 	content => "I love Puppet\n",
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
+	mode 	=> '0744',
+	owner 	=> 'www-data',
+	group 	=> 'www-data',
 }

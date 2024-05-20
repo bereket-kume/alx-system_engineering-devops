fb9096940e818619d8ea3d96d2dfd84b771b3e62
bereket kume <bereketkume@gmail.com>
2024-05-20 05:20:59 -0700

    first task is solved

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" api to fetch user data """
+
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    user_id = sys.argv[1]
+    user_data = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                             .format(user_id))
+    user_name = user_data.json().get('name')
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    total_task = comp = 0
+    for task in todo.json():
+        if task.get('userId') == int(user_id):
+            total_task += 1
+            if task.get('completed'):
+                comp += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, comp, total_task))
+
+    for task in todo.json():
+        if task.get('completed') and task.get('userId') == int(user_id):
+            print('\t' + '\n' + task.get('title'))

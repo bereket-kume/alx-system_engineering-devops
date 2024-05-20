7a4fb408eab18487596ea2e47009f8a72292b919
bereket kume <bereketkume@gmail.com>
2024-05-20 06:26:26 -0700

    done

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,7 +7,8 @@ import sys
 
 if __name__ == '__main__':
     user_id = sys.argv[1]
-    user_data = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+    user_data = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
     user_name = user_data.json().get('username')
     todo = requests.get('https://jsonplaceholder.typicode.com/todos')
     filename = f"{user_id}.csv"
@@ -15,4 +16,5 @@ if __name__ == '__main__':
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for to in todo.json():
-            writer.writerow([user_id, user_name, to.get('completed'), to.get('title')])
+            writer.writerow(
+                [user_id, user_name, to.get('completed'), to.get('title')])

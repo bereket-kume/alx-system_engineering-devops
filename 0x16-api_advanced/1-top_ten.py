307b594c4d9cfb090cf17b04167f8d37ccd6c8d9
bereket kume <bereketkume@gmail.com>
2024-06-04 08:45:47 -0700

    second task done

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+
+
+def top_ten(subreddit):
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code == 200:
+        [print(child.get('data').get('title')) for child in sub_info.json().get('data').get('children')]

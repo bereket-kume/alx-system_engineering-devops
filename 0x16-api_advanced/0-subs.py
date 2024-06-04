db8537a3fcf450ebfad26ea0a8f9fdb7e436272b
bereket kume <bereketkume@gmail.com>
2024-06-04 05:39:42 -0700

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,13 @@
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
-	import requests
+    import requests
 
-	sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-	if sub_info.status_code ==200:
-		return sub_info.json().get("data").get("subscribers")
-	else:
-		return 0
+    if sub_info.status_code == 200:
+        return sub_info.json().get("data").get("subscribers")
+    else:
+        return 0
